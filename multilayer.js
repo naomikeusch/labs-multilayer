e5c6da064263e2738e2455a09620fd373e9a6353
Andrew Martini <andrewmartini@users.noreply.github.com>
2015-08-11 13:51:22 -0400

    Update multilayer.js

--- multilayer.js
+++ multilayer.js
@@ -18,7 +18,7 @@ multilayer.controller('SelectorCtrl', function ($scope) {
             if (!show) {
                 layer.hide();
             }
-            cdb.vis.Vis.addInfowindow(map, layer.getSubLayer(0), ['cartodb_id'])
+            cdb.vis.Vis.addInfowindow(map, layer.getSubLayer(0), ['cartodb_id', 'updated_at'])
             cartodbLayers[id] = layer;
         };
     }
