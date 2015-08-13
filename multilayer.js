6c170c30c80891244538ebc68c980eb5288c2ff9
naomikeusch <naomikeusch@users.noreply.github.com>
2015-08-13 12:32:32 -0400

    Update multilayer.js

--- multilayer.js
+++ multilayer.js
@@ -60,7 +60,7 @@ multilayer.controller('SelectorCtrl', function ($scope) {
                     layer = $scope.layers[id];
                     layer.id = id;
                     //edit
-                    $scope.selectedLayers[id] = layer.show ? false : false;
+                    $scope.selectedLayers[id] = layer.show ? false : true;
                     if (layer.vizjson) {
                         layerOptions = layer.vizjson;
                     } else {
