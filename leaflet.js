49d4da298c74fadaef23500cd63a8e8dbce74d56
naomikeusch <naomikeusch@users.noreply.github.com>
2015-08-17 14:19:08 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -1,21 +1,37 @@
       function main() {
+        
+        //Testing making variables for each layer
+        //added to
+     /*   var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
+        denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
+        aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
+        golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
+        
+          var council =  L.layerGroup([littleton, denver, aurora, golden]);
+        //originally on, have to switch between
+        var grayscale = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution}),
+            streets   = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution});
+            
+        var baseMaps = {
+            "Grayscale": grayscale,
+            "Streets": streets
+        };
+        var overlayMaps = {
+            "Council": council
+        }; */
         // create leaflet map
         var map = L.map('map', { 
           zoomControl: false,
           center: [40.70, -73.97],
           zoom: 13
-        })
-        
-        //testing variables
-       /* var grayscale = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution});
-        var baseMaps = {
-        "Grayscale": grayscale
-         };
-        */
+          //include layers in definition of map
+         // layers: [grayscale, council]
+        });
         // add a base layer
         L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
           attribution: 'Stamen'
-        }).addTo(map);
+        }).addTo(map); 
+      //  L.control.layers(baseMaps, overlayMaps).addTo(map);
         // add cartodb layer with one sublayer: DONT FORGET COMMAS IN BETWEEN LAYERS IN ARRAY, AND MAKE SURE YOUR USERNAME AND SQL ARE CORRECT
             //First, layers that pop up by themselves
         cartodb.createLayer(map, {
@@ -28,7 +44,6 @@
           }
           ]
         }).addTo(map);
-        
           //Second, layers that come up by clicking maps
         cartodb.createLayer(map, {
           user_name: 'naomikeusch',
@@ -64,4 +79,4 @@
         })
       }
  
-      window.onload = main;
+      window.onload = main; 
