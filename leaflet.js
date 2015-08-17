8e40bb5b21a9d1acdd3806c8c6bfe8bed77fb48b
naomikeusch <naomikeusch@users.noreply.github.com>
2015-08-17 14:35:37 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -2,14 +2,14 @@
         
         //Testing making variables for each layer
         //added to
-        var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
+      /*  var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
         denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
         aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
         golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
         
           var council =  L.layerGroup([littleton, denver, aurora, golden]);
         //originally on, have to switch between
-      /*  var grayscale = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution}),
+        var grayscale = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution}),
             streets   = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution});
             
         var baseMaps = {
